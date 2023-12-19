c84c2477e81c59fa6f2ea1256da354a164aecf85
Théo Winterhalter <theo.winterhalter@gmail.com>
2023-12-19 14:01:57 +0100

    Move exceptions to their own file

--- theories/Examples.v
+++ theories/Examples.v
@@ -552,55 +552,10 @@ Qed.
   type.
 *)
 
-Inductive exn E A :=
-| success (x : A)
-| exception (e : E).
-
-Arguments success {E A}.
-Arguments exception {E A}.
-
-Definition exn_bind {E A B} (c : exn E A) (f : A → exn E B) :=
-  match c with
-  | success x => f x
-  | exception e => exception e
-  end.
-
-(* #[local] Instance *) Definition MonadExn {E} : Monad (exn E) := {|
-  ret A x := success x ;
-  bind A B c f := exn_bind c f
-|}.
-
-(* Exception monad transformer *)
-(* #[local] Instance *) Definition MonadExnT {E M} `{Monad M} : Monad (λ A, M (exn E A)) := {|
-  ret A x := ret (success x) ;
-  bind A B c f := bind (M := M) c (λ x,
-    match x with
-    | success y => f y
-    | exception e => ret (exception e)
-    end
-  )
-|}.
+Require Import MonadExn.
 
 Inductive error :=
 | DivisionByZero.
-
-Class MonadRaise E (M : Type → Type) := {
-  raise : ∀ (A : Type), E → M A
-}.
-
-Arguments raise {E M _ A} e.
-
-#[local] Instance MonadRaiseExnT {E M} `{Monad M} : MonadRaise E (λ A, M (exn E A)) := {|
-  raise A e := ret (exception e)
-|}.
-
-#[local] Instance OrecEffectExn E : OrecEffect (exn E).
-Proof.
-  constructor.
-  intros I H A B. apply MonadExnT.
-Defined.
-
-#[export] Existing Instance combined_monad.
 
 Equations ediv : ∇ (p : nat * nat), exn error ♯ nat :=
   ediv (n, 0) := raise DivisionByZero ;
